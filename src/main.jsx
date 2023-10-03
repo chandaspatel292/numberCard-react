1fa53f8088649b9cd56a668f56246b59fe78993f
direwolf292 <chandaspatelci@gmail.com>
2023-10-03 18:46:44 +0530

    some changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import InteractiveCard from "./InteractiveCard";
-import "./index.css";
-import NumberCard from "./NumberCard";
-import React_Card from "./React_card";
 
-import RecipeReviewCard from "../../RecipeReviewCard";
+import "./index.css";
+
+import React_Card from "./React_card";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <InteractiveCard /> */}
     <React_Card />
-    {/*  <NumberCard /> */}
-    {/*<RecipeReviewCard />*/}
   </React.StrictMode>
 );
